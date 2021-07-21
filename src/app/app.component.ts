b24b9373cdfa7b6ba6683aca9683cefce4532ea9
Caitlin Allie <callie@vertafore.com>
2021-07-20 21:41:02 -0400

    Initial commit for DAWG Days 2021 Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'angular-reactive-forms';
+export class AppComponent implements OnInit {
+    signupForm!: FormGroup;
+    addressForm!: FormGroup;
+    phoneForm!: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+    }
+
+    ngOnInit(): any {
+        this.signupForm = this.formBuilder.group({
+            email: ['', [
+                Validators.required,
+                Validators.email
+            ]],
+            password: ['', [
+                Validators.required,
+                Validators.minLength(4)
+            ]],
+            age: ['', [
+                Validators.required,
+                Validators.min(18)
+            ]],
+            agreeToTerms: [false, [
+                Validators.requiredTrue
+            ]]
+        });
+
+        this.addressForm = this.formBuilder.group({
+            homeAddress: this.formBuilder.group({
+                street: '',
+                city: '',
+                state: ''
+            }),
+            businessAddress: this.formBuilder.group({
+                street: '',
+                city: '',
+                state: ''
+            })
+        });
+
+        const phone = this.formBuilder.group({
+            area: [],
+            prefix: [],
+            line: []
+        });
+
+        this.phoneForm = this.formBuilder.group({
+            phones: this.formBuilder.array([phone])
+        });
+    }
+
+    onSubmit(form: FormGroup): void {
+        console.log(form.value);
+    }
+
+    resetForm(form: FormGroup): void {
+        form.reset();
+    }
+    get phoneForms(): FormArray {
+        return this.phoneForm.get('phones') as FormArray;
+    }
+
+    addPhone(): void {
+        const phone = this.formBuilder.group({
+            area: [],
+            prefix: [],
+            line: []
+        });
+        this.phoneForms.push(phone);
+    }
+
+    removePhone(i: number): void {
+        this.phoneForms.removeAt(i);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
